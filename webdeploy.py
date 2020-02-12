91285723b9a676ec018cc4cb8f3e0ddc40db45f9
Valentin Vareskic <valentin.vareskic@outlook.com>
2020-02-12 12:23:23 +0100

    Test

--- webdeploy.py
+++ webdeploy.py
@@ -3,11 +3,14 @@ import os
 
 
 def webDeploy(commitMsg):
+    os.system('cmd /c "ng build --prod --output-path docs --base-href /portfolio/"')
     os.system('cmd /c "git status "')
-    print("status up there")
     os.system('cmd /c "git add . "')
     print("I have added some shit")
     os.system('cmd /c "git commit -m %s "'%commitMsg)
+    os.system('cmd /c "git push "')
+    os.system('cmd /c "cd docs "')
+
 
 
 

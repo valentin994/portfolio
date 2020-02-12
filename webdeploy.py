db9a4d933798ddce7e04e84405aa5e9574a0c322
Valentin Vareskic <valentin.vareskic@outlook.com>
2020-02-12 12:52:32 +0100

    Automated

--- webdeploy.py
+++ webdeploy.py
@@ -1,15 +1,18 @@
 import sys
 import os
 
+BASE_DIR = os.getcwd()
 
 def webDeploy(commitMsg):
     os.system('cmd /c "ng build --prod --output-path docs --base-href /portfolio/"')
+    os.chdir(BASE_DIR + "\docs")
+    os.system('cmd /c "copy index.html 404.html"')
+    os.chdir(BASE_DIR)
     os.system('cmd /c "git status "')
     os.system('cmd /c "git add . "')
     print("I have added some shit")
     os.system('cmd /c "git commit -m %s "'%commitMsg)
     os.system('cmd /c "git push "')
-    os.system('cmd /c "cd docs "')
 
 
 

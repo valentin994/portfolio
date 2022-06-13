ad4253fb4c4d4824855e2170f79dd897d34319cb
valentin994 <valentin.vareskic@outlook.com>
2022-06-13 19:12:39 +0200

    added menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+//TODO Add icons for github, linkedid, possible youtube
+</script>
+<template>
+  <div class="flex p-4">
+    <div class="flex-none">
+      <img alt="Valentin Vareskic logo" src="../assets/logo_nacrt.png" class="h-auto w-16"/>
+    </div>
+    <div class="flex-1"></div>
+    <ul class="flex flex-row-reverse p-4 justify-between border-black text-slate-700">
+      <li class="pl-2">GitHub</li>
+      <li class="pl-2">LinkedIn</li>
+      <li class="pl-2">About me</li>
+      <li class="pl-2">Skills</li>
+      <li class="pl-2">Contact</li>
+    </ul>
+  </div>
+</template>
